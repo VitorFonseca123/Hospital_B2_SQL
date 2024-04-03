6fa0ea451e5cef10dd44d43e4069619dbf9c36b8
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2024-04-03 16:21:41 -0300

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,4 +1,4 @@
-  /*CREATE DOMAIN IF NOT EXISTS generoDomain AS smallint
+  CREATE DOMAIN IF NOT EXISTS generoDomain AS smallint
   CHECK (VALUE BETWEEN 1 AND 6);
 
   	1- Homem-Cis
@@ -7,7 +7,12 @@
 	4- Mulher-Trans
 	5- Não binario
 	6 - outro
-  */
+  
+  
+  /* SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public'
+AND table_type = 'BASE TABLE';*/
 CREATE TABLE IF NOT EXISTS Paciente (
     Num_Registro serial ,
     CPF int NOT NULL UNIQUE,
@@ -22,7 +27,7 @@ CREATE TABLE IF NOT EXISTS Telefone (
     Num_Registro int,
     Num_Contato int,
 	
-	CONSTRAINT chaveTelefone PRIMARY KEY(Num_Contato, Num_Registro),
+	CONSTRAINT chaveTelefone PRIMARY KEY(Num_Contato),
 	CONSTRAINT FK_Num_Registro_Telefone FOREIGN KEY(Num_Registro) REFERENCES Paciente(Num_Registro) 
 	ON DELETE CASCADE ON UPDATE CASCADE	
 );
@@ -30,7 +35,17 @@ CREATE TABLE IF NOT EXISTS Prontuario (
     Num_Registro int,
     Num_Prontuario serial,
 	
-	CONSTRAINT chaveProntuario PRIMARY KEY(Num_Prontuario, Num_Registro),
+	CONSTRAINT chaveProntuario PRIMARY KEY(Num_Prontuario),
 	CONSTRAINT FK_Num_Registro_Prontuario FOREIGN KEY(Num_Registro) REFERENCES Paciente(Num_Registro) 
+	ON DELETE CASCADE ON UPDATE CASCADE	
+);
+
+CREATE TABLE IF NOT EXISTS Observacao_Medica (
+	Id_Observacao serial,
+    Observacao text,
+    Num_Prontuario int,
+	
+	CONSTRAINT chaveObservacao PRIMARY KEY(Num_Prontuario),
+	CONSTRAINT FK_Num_Prontuario_Observacao FOREIGN KEY(Num_Prontuario) REFERENCES Prontuario(Num_Prontuario) 
 	ON DELETE CASCADE ON UPDATE CASCADE	
 );
