dda6bc39619c184effc2cc9cf62a393791a19a63
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2024-04-03 17:01:02 -0300

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,4 +1,5 @@
-  /*CREATE DOMAIN  generoDomain AS smallint
+ /*CREATE DATABASE  HospitalBD2;
+ CREATE DOMAIN  generoDomain AS smallint
   CHECK (VALUE BETWEEN 1 AND 6);
 
   	1- Homem-Cis
@@ -13,18 +14,20 @@
 FROM information_schema.tables
 WHERE table_schema = 'public'
 AND table_type = 'BASE TABLE';*/
+
+/*PACIENTE*/
 CREATE TABLE IF NOT EXISTS Paciente (
     Num_Registro serial ,
     CPF int NOT NULL UNIQUE,
     Nome_Paciente varchar(50) NOT NULL,
     Sobrenome_Paciente varchar(50) NOT NULL,
-    Data_Nascimento date,
-    Genero generoDomain,
+    Data_Nascimento date NOT NULL,
+    Genero generoDomain NOT NULL,
 	
 	CONSTRAINT RegistroPaciente PRIMARY KEY(Num_Registro)
 );
 CREATE TABLE IF NOT EXISTS Telefone (
-    Num_Registro int,
+    Num_Registro int NOT NULL,
     Num_Contato int,
 	
 	CONSTRAINT chaveTelefone PRIMARY KEY(Num_Contato),
@@ -32,7 +35,7 @@ CREATE TABLE IF NOT EXISTS Telefone (
 	ON DELETE CASCADE ON UPDATE CASCADE	
 );
 CREATE TABLE IF NOT EXISTS Prontuario (
-    Num_Registro int,
+    Num_Registro int NOT NULL,
     Num_Prontuario serial,
 	
 	CONSTRAINT chaveProntuario PRIMARY KEY(Num_Prontuario),
@@ -42,8 +45,8 @@ CREATE TABLE IF NOT EXISTS Prontuario (
 
 CREATE TABLE IF NOT EXISTS Observacao_Medica (
 	Id_Observacao serial,
-    Observacao text,
-    Num_Prontuario int,
+    Observacao text NOT NULL,
+    Num_Prontuario int NOT NULL,
 	
 	CONSTRAINT chaveObservacao PRIMARY KEY(Num_Prontuario),
 	CONSTRAINT FK_Num_Prontuario_Observacao FOREIGN KEY(Num_Prontuario) REFERENCES Prontuario(Num_Prontuario) 
@@ -51,10 +54,34 @@ CREATE TABLE IF NOT EXISTS Observacao_Medica (
 );
 CREATE TABLE IF NOT EXISTS Diagnostico (
 	Id_Diagnostico serial,
-    Diagnostico varchar(30), /*acho que ta grande*/
-    Num_Prontuario int,
+    Diagnostico varchar(30) NOT NULL, /*acho que ta grande*/
+    Num_Prontuario int NOT NULL,
 	
 	CONSTRAINT chaveDiagnostico PRIMARY KEY(Id_Diagnostico),
 	CONSTRAINT FK_Num_Prontuario_Observacao FOREIGN KEY(Num_Prontuario) REFERENCES Prontuario(Num_Prontuario) 
 	ON DELETE CASCADE ON UPDATE CASCADE	
 );
+
+
+/*MEDICO*/
+
+CREATE DOMAIN EspecialidadesDomain AS VARCHAR(21)
+    CHECK (VALUE IN ('Cardiologia', 'Dermatologia', 'Pediatria', 'Psiquiatria', 'Ortopedia', 'Ginecologia', 'Urologia',
+                      'Oftalmologia', 'Otorrinolaringologia', 'Endocrinologia', 'Neurologia', 'Cirurgia Geral',
+                      'Cirurgia Plástica', 'Cirurgia Vascular', 'Radiologia', 'Anestesiologia', 'Oncologia',
+                      'Gastroenterologia', 'Hematologia', 'Infectologia', 'Nefrologia', 'Reumatologia'));
+
+CREATE TABLE IF NOT EXISTS Medico (
+    COD_CRM INT,
+	Estado char(2),
+	Nome_Medico varchar(50) NOT NULL,
+    Sobrenome_Medico varchar(50) NOT NULL,
+	Especialidade1 EspecialidadesDomain NOT NULL,
+	Especialidade2 EspecialidadesDomain CHECK (Especialidade2 <> Especialidade1),
+	
+	
+    CONSTRAINT tamanhoCRM CHECK (COD_CRM >= 0 AND COD_CRM < 1000000),
+	CONSTRAINT chaveMedico PRIMARY KEY(COD_CRM,Estado)
+	
+);
+
