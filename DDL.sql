ab25c182afe7207a499b7af6b942019a58ec1930
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2024-04-04 16:54:09 -0300

    Arruma problemas

--- DDL.sql
+++ DDL.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS Genero(
 	Id_Genero serial,
-	Nome_Genero varchar(10)
+	Nome_Genero varchar(10),
 	
+	CONSTRAINT chaveGenero PRIMARY KEY(Id_Genero)
 );
 CREATE TABLE IF NOT EXISTS Paciente (
     Num_Registro serial ,
@@ -9,7 +10,7 @@ CREATE TABLE IF NOT EXISTS Paciente (
     Nome_Paciente varchar(50) NOT NULL,
     Sobrenome_Paciente varchar(50) NOT NULL,
     Data_Nascimento date NOT NULL,
-    GeneroPaciente varchar(10)
+    GeneroPaciente int,
 	
 	CONSTRAINT RegistroPaciente PRIMARY KEY(Num_Registro),
 	CONSTRAINT GeneroPaciente_FK FOREIGN KEY(GeneroPaciente) REFERENCES Genero(Id_Genero) 
@@ -20,48 +21,50 @@ CREATE TABLE IF NOT EXISTS Telefone (
     Num_Contato int,
 	
 	CONSTRAINT FK_Num_Registro_Telefone FOREIGN KEY(Num_Registro) REFERENCES Paciente(Num_Registro) 
-		ON DELETE CASCADE ON UPDATE CASCADE	
-	CONSTRAINT chaveTelefone PRIMARY KEY(Num_Contato, Num_Registro),
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT chaveTelefone PRIMARY KEY(Num_Contato, Num_Registro)
 );
 CREATE TABLE IF NOT EXISTS Prontuario (
     Num_Registro int NOT NULL,
     Num_Prontuario serial,
 	
+	CONSTRAINT chaveProntuario PRIMARY KEY(Num_Registro, Num_Prontuario),
 	CONSTRAINT FK_Num_Registro_Prontuario FOREIGN KEY(Num_Registro) REFERENCES Paciente(Num_Registro) 
-		ON DELETE CASCADE ON UPDATE CASCADE	
-	CONSTRAINT chaveProntuario PRIMARY KEY(Num_Prontuario, Num_Registro),
+		ON DELETE CASCADE ON UPDATE CASCADE
+	
 );
 
 CREATE TABLE IF NOT EXISTS Observacao_Medica (
-	Id_Observacao serial,
+    Id_Observacao serial PRIMARY KEY,
     Observacao text NOT NULL,
     Num_Prontuario int NOT NULL,
+	Num_Registro int NOT NULL,
 	
-	CONSTRAINT chaveObservacao PRIMARY KEY(Id_Observacao),
-	CONSTRAINT FK_Num_Prontuario_Observacao FOREIGN KEY(Num_Prontuario) REFERENCES Prontuario(Num_Prontuario) 
-		ON DELETE CASCADE ON UPDATE CASCADE	
+	CONSTRAINT FK_Num_Prontuario_Observacao FOREIGN KEY(Num_Registro, Num_Prontuario) 
+		REFERENCES Prontuario(Num_Registro, Num_Prontuario) 
+		ON DELETE CASCADE ON UPDATE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS Diagnostico (
-	Id_Diagnostico serial,
-    Diagnostico varchar(30) NOT NULL, /*acho que ta grande*/
+    Id_Diagnostico serial PRIMARY KEY,
+    Diagnostico varchar(30) NOT NULL,
     Num_Prontuario int NOT NULL,
+	Num_Registro int NOT NULL,
 	
-	CONSTRAINT chaveDiagnostico PRIMARY KEY(Id_Diagnostico),
-	CONSTRAINT FK_Num_Prontuario_Observacao FOREIGN KEY(Num_Prontuario) REFERENCES Prontuario(Num_Prontuario) 
-		ON DELETE CASCADE ON UPDATE CASCADE	
+	CONSTRAINT FK_Num_Prontuario_Diagnostico FOREIGN KEY(Num_Registro, Num_Prontuario) 
+		REFERENCES Prontuario(Num_Registro, Num_Prontuario) 
+		ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
 /*MEDICO*/
 
-/*CREATE DOMAIN EspecialidadesDomain AS VARCHAR(21)
-    CHECK (VALUE IN ('Cardiologia', 'Dermatologia', 'Pediatria', 'Psiquiatria', 'Ortopedia', 'Ginecologia', 'Urologia',
-                      'Oftalmologia', 'Otorrinolaringologia', 'Endocrinologia', 'Neurologia', 'Cirurgia Geral',
-                      'Cirurgia Plástica', 'Cirurgia Vascular', 'Radiologia', 'Anestesiologia', 'Oncologia',
-                      'Gastroenterologia', 'Hematologia', 'Infectologia', 'Nefrologia', 'Reumatologia'));
-*/
 CREATE TABLE IF NOT EXISTS Especialidade(
+	Id_Especialidade serial,
+	Nome_Especialidade varchar(21) NOT NULL,
+	Descricao_Esp varchar(50) NOT NULL,
 	
+	CONSTRAINT chaveEspecialidade PRIMARY KEY(Id_Especialidade)
 
 );
 CREATE TABLE IF NOT EXISTS Medico (
@@ -69,13 +72,16 @@ CREATE TABLE IF NOT EXISTS Medico (
 	Estado char(2),
 	Nome_Medico varchar(50) NOT NULL,
     Sobrenome_Medico varchar(50) NOT NULL,
-	Especialidade1 EspecialidadesDomain NOT NULL,
-	Especialidade2 EspecialidadesDomain CHECK (Especialidade2 <> Especialidade1),
+	Especialidade1_FK int,
+	Especialidade2_FK int CHECK (Especialidade2_FK <> Especialidade1_FK),
 	
 	CONSTRAINT chaveMedico PRIMARY KEY(COD_CRM,Estado),
-    CONSTRAINT tamanhoCRM CHECK (COD_CRM >= 0 AND COD_CRM < 1000000)
+    CONSTRAINT tamanhoCRM CHECK (COD_CRM >= 0 AND COD_CRM < 1000000),
 	
-	
+	CONSTRAINT FK_Num_Esp1 FOREIGN KEY(Especialidade1_FK) REFERENCES Especialidade(Id_Especialidade)
+		ON DELETE SET NULL ON UPDATE CASCADE,
+	CONSTRAINT FK_Num_Esp2 FOREIGN KEY(Especialidade2_FK) REFERENCES Especialidade(Id_Especialidade) 
+		ON DELETE SET NULL ON UPDATE CASCADE
 );
 /*DEPOSITO*/
 CREATE TABLE Deposito(
@@ -85,12 +91,4 @@ CREATE TABLE Deposito(
 	
 	CONSTRAINT chaveDeposito PRIMARY KEY(Id_Deposito),
 	CONSTRAINT MaxAndares CHECK (AndarDep > 0 AND AndarDep < 8)
-);
-CREATE TABLE Material(
-	Id_Material,
-	Marca_Registrada,
-	Lote,
-	Qtd_Atual,
-	
-	
 );
