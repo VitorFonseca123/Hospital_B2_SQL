091a399c5c879852baf185be3a931be550501692
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2024-04-04 20:34:22 -0300

    Update INSERT GENERO.sql

--- INSERT GENERO.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO Genero (Nome_Genero)values('Homem-Cis');
-INSERT INTO Genero (Nome_Genero)values('Mulher-Cis');
-INSERT INTO Genero (Nome_Genero)values('Homem-Trans');
-INSERT INTO Genero (Nome_Genero)values('Mulher-Trans');
-INSERT INTO Genero (Nome_Genero)values('Não-Binário');
-INSERT INTO Genero (Nome_Genero)values('Outro');
