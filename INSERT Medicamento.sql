15a4d1b647e1bedd6e7a3c25098700a0bab2b791
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2024-04-07 20:12:48 -0300

    Update INSERT Medicamento.sql

--- INSERT Medicamento.sql
+++ INSERT Medicamento.sql
@@ -4,7 +4,7 @@ WITH new_material AS (
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Paracetamol', 'Acetaminophen', '2025-01-01', 'Analgésico', 'ABC Laboratories'
+SELECT Id_Material, 'Paracetamol', 'Acetaminophen', '2025-01-01', 'Analgésico', 'ABCLaboratories'
 FROM new_material;
 
 WITH new_material AS (
@@ -13,7 +13,7 @@ WITH new_material AS (
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Ibuprofeno', 'Ibuprofen', '2024-12-01', 'Anti-inflamatório', 'XYZ Pharmaceuticals'
+SELECT Id_Material, 'Ibuprofeno', 'Ibuprofen', '2024-12-01', 'Anti-inflamatório', 'XYZFarma'
 FROM new_material;
 
 WITH new_material AS (
@@ -22,7 +22,7 @@ WITH new_material AS (
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Dipirona', 'Metamizole', '2024-11-15', 'Analgésico', 'DEF Pharmaceuticals'
+SELECT Id_Material, 'Dipirona', 'Metamizole', '2024-11-15', 'Analgésico', 'DEFPharma'
 FROM new_material;
 
 WITH new_material AS (
@@ -31,7 +31,7 @@ WITH new_material AS (
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Dexametasona', 'Dexamethasone', '2024-10-20', 'Corticosteroide', 'GHI Laboratories'
+SELECT Id_Material, 'Dexametasona', 'Dexamethasone', '2024-10-20', 'Corticosteroide', 'GHILab'
 FROM new_material;
 
 WITH new_material AS (
@@ -40,7 +40,7 @@ WITH new_material AS (
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Omeprazol', 'Omeprazole', '2024-09-15', 'Inib.Bomba de Prótons', 'JKL Pharmaceuticals'
+SELECT Id_Material, 'Omeprazol', 'Omeprazole', '2024-09-15', 'Inib.Bomba de Prótons', 'JKLFarma'
 FROM new_material;
 
 WITH new_material AS (
@@ -49,132 +49,123 @@ WITH new_material AS (
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Ibuprofeno', 'Ibuprofen', '2025-05-20', 'Anti-inflamatório', 'AdvilPharmaceuticals'
+SELECT Id_Material, 'Ibuprofeno', 'Ibuprofen', '2025-05-20', 'Ant-inflama', 'Adceuticals'
 FROM new_material;
 
 WITH new_material AS (
     INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Paracetamol 500mg', 'Tylenol', 'DEF456', 100)
+    VALUES ('Dipirona', 'Novalgina', 'GHI789', 30)
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Paracetamol', 'Acetaminophen', '2023-12-10', 'Analgésico', 'Tylenol Pharmaceuticals'
+SELECT Id_Material, 'Dipirona', 'Metamizole', '2024-08-30', 'Analgésico', 'Pharmaceuticals'
 FROM new_material;
 
 WITH new_material AS (
     INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Dipirona 1g', 'Novalgina', 'GHI789', 30)
+    VALUES ('Ondansetron', 'Zofran', 'MNO012', 10)
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Dipirona', 'Metamizole', '2024-08-30', 'Analgésico', 'Novalgina Pharmaceuticals'
+SELECT Id_Material, 'Ondansetron', 'Ondansetron', '2024-11-25', 'Antiemético', 'Pharmaceuticals'
 FROM new_material;
 
 WITH new_material AS (
     INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Ondansetron 4mg', 'Zofran', 'MNO012', 10)
+    VALUES ('Simeticona', 'Luftal', 'PQR345', 25)
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Ondansetron', 'Ondansetron', '2024-11-25', 'Antiemético', 'Zofran Pharmaceuticals'
+SELECT Id_Material, 'Simeticona', 'Simethicone', '2023-09-15', 'Antiflatulento', 'Luftal Pharma'
 FROM new_material;
 
 WITH new_material AS (
     INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Simeticona 125mg', 'Luftal', 'PQR345', 25)
+    VALUES ('Amoxicilina', 'Amoxil', 'STU678', 15)
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Simeticona', 'Simethicone', '2023-09-15', 'Antiflatulento', 'Luftal Pharmaceuticals'
+SELECT Id_Material, 'Amoxicilina', 'Amoxicillin', '2023-06-30', 'Antibiótico', 'Amoxticals'
 FROM new_material;
 
 WITH new_material AS (
     INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Amoxicilina 500mg', 'Amoxil', 'STU678', 15)
+    VALUES ('Cetirizina', 'Zyrtec', 'VWX901', 40)
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Amoxicilina', 'Amoxicillin', '2023-06-30', 'Antibiótico', 'Amoxil Pharmaceuticals'
+SELECT Id_Material, 'Cetirizina', 'Cetirizine', '2024-03-15', 'Antialérgico', 'Zyrtec'
 FROM new_material;
 
 WITH new_material AS (
     INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Cetirizina 10mg', 'Zyrtec', 'VWX901', 40)
+    VALUES ('Sinvastatina', 'Zocor', 'YZA234', 12)
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Cetirizina', 'Cetirizine', '2024-03-15', 'Antialérgico', 'Zyrtec Pharmaceuticals'
+SELECT Id_Material, 'Sinvastatina', 'Simvastatin', '2025-01-20', 'Hipo-lipemiante', 'Zocticals'
 FROM new_material;
 
 WITH new_material AS (
     INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Sinvastatina 20mg', 'Zocor', 'YZA234', 12)
+    VALUES ('Dexametasona', 'Decadron', 'ZAB567', 8)
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Sinvastatina', 'Simvastatin', '2025-01-20', 'Hipo-lipemiante', 'Zocor Pharmaceuticals'
+SELECT Id_Material, 'Dexametasona', 'Dexamethasone', '2024-07-10', 'Corticosteroide', 'Decadicals'
 FROM new_material;
 
 WITH new_material AS (
     INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Dexametasona 4mg', 'Decadron', 'ZAB567', 8)
+    VALUES ('Ranitimed', 'Zantac', 'ZCD678', 30)
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Dexametasona', 'Dexamethasone', '2024-07-10', 'Corticosteroide', 'Decadron Pharmaceuticals'
+SELECT Id_Material, 'Ranitidina', 'Ranitidine', '2023-08-05', 'AntagonistaH2', 'Zanteuticals'
 FROM new_material;
 
 WITH new_material AS (
     INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Ranitidina 150mg', 'Zantac', 'ZCD678', 30)
+    VALUES ('Levotimed', 'Synthroid', 'ZEF901', 20)
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Ranitidina', 'Ranitidine', '2023-08-05', 'Antagonista H2', 'Zantac Pharmaceuticals'
+SELECT Id_Material, 'Levotiroxina', 'Levothyroxine', '2024-12-15', 'Hormônio Tireoidiano', 'Synthroid Pharma'
 FROM new_material;
 
 WITH new_material AS (
     INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Levotiroxina 100mcg', 'Synthroid', 'ZEF901', 20)
+    VALUES ('Sertralina', 'Zoloft', 'ZGH123', 25)
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Levotiroxina', 'Levothyroxine', '2024-12-15', 'Hormônio Tireoidiano', 'Synthroid Pharmaceuticals'
+SELECT Id_Material, 'Sertralina', 'Sertraline', '2024-10-20', 'Antidepressivo', 'ZoloftPharma'
 FROM new_material;
 
 WITH new_material AS (
     INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Sertralina 50mg', 'Zoloft', 'ZGH123', 25)
+    VALUES ('Clonazepam', 'Klonopin', 'ZYX456', 10)
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Sertralina', 'Sertraline', '2024-10-20', 'Antidepressivo', 'Zoloft Pharmaceuticals'
+SELECT Id_Material, 'Clonazepam', 'Clonazepam', '2023-11-30', 'Ansiolítico', 'Klonop'
 FROM new_material;
 
 WITH new_material AS (
     INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Clonazepam 2mg', 'Klonopin', 'ZYX456', 10)
+    VALUES ('Metformed', 'Glucophage', 'ZWX789', 18)
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Clonazepam', 'Clonazepam', '2023-11-30', 'Ansiolítico', 'Klonopin Pharmaceuticals'
+SELECT Id_Material, 'Metformina', 'Metformin', '2025-02-28', 'Antidiabético', 'GlucopPharma'
 FROM new_material;
 
 WITH new_material AS (
     INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Metformina 850mg', 'Glucophage', 'ZWX789', 18)
+    VALUES ('Loratadina', 'Claritin', 'ZXY567', 35)
     RETURNING Id_Material
 )
 INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Metformina', 'Metformin', '2025-02-28', 'Antidiabético Oral', 'Glucophage Pharmaceuticals'
-FROM new_material;
-
-WITH new_material AS (
-    INSERT INTO Material (Nome_Material, Marca_Registrada, Lote, Qtd_Atual)
-    VALUES ('Loratadina 10mg', 'Claritin', 'ZXY567', 35)
-    RETURNING Id_Material
-)
-INSERT INTO Medicamento (Id_Material, Nome_Generico, Nome_Quimico, Validade, Grupo_Terapeutico, Laboratorio)
-SELECT Id_Material, 'Loratadina', 'Loratadine', '2023-07-25', 'Antialérgico', 'Claritin Pharmaceuticals'
+SELECT Id_Material, 'Loratadina', 'Loratadine', '2023-07-25', 'Antialérgico', 'ClartPharm'
 FROM new_material;
 
