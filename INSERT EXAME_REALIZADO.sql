45f5684e1e447e29445feebaa26d343463e8f5a7
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2024-04-09 17:06:18 -0300

    Create INSERT EXAME_REALIZADO.sql

---
+++ INSERT EXAME_REALIZADO.sql
@@ -0,0 +1,11 @@
+WITH new_procedimentos AS (
+    INSERT INTO Procedimento (num_registro_pac, Data_Hora_Entrada, Data_Hora_Saida)
+    SELECT FLOOR(RANDOM() * 100) + 1, CURRENT_TIMESTAMP - INTERVAL '10 days' * RANDOM(),
+           CURRENT_TIMESTAMP - INTERVAL '10 days' * RANDOM()
+    FROM generate_series(1, 100)
+    RETURNING Id_Procedimento
+)
+INSERT INTO Exame_Realizado (Id_Procedimento, Id_exame)
+	SELECT np.Id_Procedimento, 
+	FLOOR(RANDOM() * 100) + 1
+FROM new_procedimentos np;
