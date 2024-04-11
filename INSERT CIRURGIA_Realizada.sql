c5ec8d90cc0fe3bdacaac47692f93561676663d3
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2024-04-11 16:40:34 -0300

    Update INSERT CIRURGIA_Realizada.sql

--- INSERT CIRURGIA_Realizada.sql
+++ INSERT CIRURGIA_Realizada.sql
@@ -1,16 +1,14 @@
 WITH new_procedimento AS (
-    INSERT INTO Procedimento (num_registro_pac, Data_Hora_Entrada , Data_Hora_Saida)
-    VALUES (FLOOR(RANDOM() * 100) + 1, CURRENT_TIMESTAMP - INTERVAL '10 days' * RANDOM(),
-    CURRENT_TIMESTAMP - INTERVAL '10 days' * RANDOM())
+    INSERT INTO Procedimento (num_registro_pac, Data_Hora_Entrada)
+    VALUES (FLOOR(RANDOM() * 100) + 1, CURRENT_TIMESTAMP - INTERVAL '10 days' * RANDOM())
     RETURNING Id_Procedimento
 )
 INSERT INTO Cirurgia_Realizada (Id_Procedimento, Complicacao, Gravidade, Id_Cirurgia)
 SELECT Id_Procedimento, 'Sem complicacoes', 'Baixa', FLOOR(RANDOM() * 24) + 1
 FROM new_procedimento;
 WITH new_procedimentos AS (
-    INSERT INTO Procedimento (num_registro_pac, Data_Hora_Entrada, Data_Hora_Saida)
-    SELECT FLOOR(RANDOM() * 100) + 1, CURRENT_TIMESTAMP - INTERVAL '10 days' * RANDOM(),
-           CURRENT_TIMESTAMP - INTERVAL '10 days' * RANDOM()
+    INSERT INTO Procedimento (num_registro_pac, Data_Hora_Entrada)
+    SELECT FLOOR(RANDOM() * 100) + 1, CURRENT_TIMESTAMP - INTERVAL '10 days' * RANDOM()
     FROM generate_series(1, 100)
     RETURNING Id_Procedimento
 )
